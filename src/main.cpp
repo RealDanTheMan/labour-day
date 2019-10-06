bf5c18fd3c35e3bab9ae8278d739180e1ae89e3d
MrDanTheMan <dantheman.py@gmail.com>
2019-10-06 18:36:41 +0100

    Cleaned up main function

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,24 @@
 #include "engine/enginecore.hpp"
 #include "engine/window.hpp"
 
+#include <assert.h>
+
 int main()
 {
-    Engine::EngineCore core = Engine::EngineCore();
-    core.Initialize();
 
-    std::unique_ptr<Engine::Window> win = std::make_unique<Engine::Window>();
-    while(win->Ready())
+    Engine::EngineCore core = Engine::EngineCore();
+    Engine::EngineCoreSettings settings = Engine::EngineCoreSettings();
+    settings.m_windowWidth = 1280;
+    settings.m_windowHeight = 720;
+
+    if(!core.Initialize(settings))
     {
+        fprintf(stderr, "Failed to initialize engine core !\n");
+    }
+
+    while(core.MainWindowActive())
+    {
+        core.RedrawMainWindow();
     }
 
     core.Terminate();
