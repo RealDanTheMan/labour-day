7452bf8726badf745fa661a1f0f417e866e24519
MrDanTheMan <dantheman.py@gmail.com>
2019-10-15 00:29:49 +0100

    Implemented basic runtime shaders

---
+++ src/engine/runtimeshaders.cpp
@@ -0,0 +1,51 @@
+#include "runtimeshaders.hpp"
+#include "debugging.hpp"
+
+using namespace Engine;
+
+void RuntimeShaders::Init()
+{
+    assert (FLAT_WHITE_VERT_SRC != nullptr);
+    assert (FLAT_WHITE_PIX_SRC != nullptr);
+
+    std::unique_ptr<ShaderSource> flatWhiteVS = std::make_unique<ShaderSource>(std::string(FLAT_WHITE_VERT_SRC), ShaderSource::ShaderSourceType::Vertex);
+    std::unique_ptr<ShaderSource> flatWhitePS = std::make_unique<ShaderSource>(std::string(FLAT_WHITE_PIX_SRC), ShaderSource::ShaderSourceType::Pixel);
+    flatWhiteVS->Compile();
+    flatWhitePS->Compile();
+
+    D_MSG(flatWhiteVS->GetLog());
+    D_MSG(flatWhitePS->GetLog());
+
+    assert (flatWhiteVS->Compiled());
+    assert (flatWhitePS->Compiled());
+
+    m_flatWhite = std::make_unique<ShaderProg>(*flatWhiteVS.get(), *flatWhitePS.get());
+    assert (m_flatWhite->Ready());
+
+    flatWhiteVS->Free();
+    flatWhitePS->Free();
+}
+
+void RuntimeShaders::Free()
+{
+    if(m_flatWhite != nullptr)
+    {
+        m_flatWhite->Free();
+        m_flatWhite.reset();
+    }
+}
+
+const bool RuntimeShaders::Ready() const 
+{
+    if(m_flatWhite == nullptr || !m_flatWhite->Ready())
+    {
+        return true;
+    }
+
+    return true;
+}
+
+const ShaderProg* RuntimeShaders::FlatWhite() const
+{
+    return m_flatWhite.get();
+}
---
+++ src/engine/runtimeshaders.hpp
@@ -0,0 +1,44 @@
+#ifndef RUNTIME_SHADERS_H_
+#define RUNTIME_SHADERS_H_
+
+#include "shaderprog.hpp"
+#include <memory>
+
+namespace Engine
+{
+    class RuntimeShaders
+    {
+        public:
+        void Init();
+        void Free();
+        const bool Ready() const ;
+
+        const ShaderProg* FlatWhite() const;
+
+        private:
+        std::unique_ptr<ShaderProg> m_flatWhite;
+    };
+
+    const char* const FLAT_WHITE_VERT_SRC = R"(
+        #version 330
+
+        layout(location = 0) in vec3 SV_VERTEX;
+
+        void main(void)
+        {
+            gl_Position = vec4(SV_VERTEX.x, SV_VERTEX.y, SV_VERTEX.z, 1.0);
+        }
+    )";
+
+    const char* const FLAT_WHITE_PIX_SRC = R"(
+        #version 330
+
+        out vec4 SV_OUT_COLOR;
+        void main()
+        {
+            SV_OUT_COLOR = vec4(1, 1, 1, 1);
+        }
+    )";
+}
+
+#endif
