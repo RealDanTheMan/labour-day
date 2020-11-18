fcf52e6e6c333fa038aec895ff45b3657ea3f08e
dantheman <dantheman.py@gmail.com>
2020-11-18 01:31:23 +0000

    Added extra assert to test proc update to validate model handle

--- src/testproc.cpp
+++ src/testproc.cpp
@@ -11,6 +11,7 @@ void TestProc::OnUpdate(Engine::Entity * const entity)
 {
     auto cmodel = entity->Components().Get<Engine::Components::ModelComponent>();
     assert (cmodel != nullptr);
+    assert (cmodel->ModelHandle() != nullptr);
 
     //cmodel->ModelHandle()->GetTransform().Scale(0.999);
     cmodel->ModelHandle()->GetTransform().Rotate(Vec3(1.0f, 1.0f, 1.0f));
